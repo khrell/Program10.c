28daded74cd46fd45c3ea88ca6f5018164f6d59b
riptorn01@gmail.com <Trinity1>
2016-11-08 21:42:42 -0600

    Rev 2 working on part 3

--- Program10.c/Program10.c
+++ Program10.c/Program10.c
@@ -15,23 +15,33 @@ typedef enum  {TRUE,FALSE} boolean_t;
 
 
 boolean_t is_letter(char letter);
+int count_words(char string1[], int i);
 
 
 int main()
 {
+	//part 2
 	char letter = ' ';
 	is_letter(letter);
-
+	
+	//part 3
+	char string1[200];
+	int i = 0;
+	int count = count_words(string1,i);
+	printf("Length of string: %d\n", count);
 
 	return 0;
 }
+// Part 2 Takes input and checks to see if it's in the alphabet
 boolean_t is_letter(char letter)
 {
+	//two variables created for return
 	boolean_t myBoolTrue = { TRUE };
 	boolean_t myBoolFalse = { FALSE };
 	printf("Please enter a single character of the alphabet: ");
 		scanf("%s", &letter);
 
+		//loop to check it the character is in the alphabet, lowcase and uppercase
 		if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
 		{
 			return myBoolTrue;
@@ -43,3 +53,23 @@ boolean_t is_letter(char letter)
 			
 		}
 }
+
+//part 3
+int count_words(char string1[], int i)
+{
+
+	int count = 0;
+	int x = '\0';
+	printf("Enter a string: ");
+	scanf("%s", string1);
+
+	while (string1[i] != x)
+	{
+		if (string1[i] == ' ')
+		{
+			count++;
+		}		
+	}
+	return count;
+}
+
